bb4e095a70e185b682e492ea4f6fed4d3c2019e0
911_Arba_Catalina <91603584+CatalinaArba@users.noreply.github.com>
2023-03-26 11:36:18 +0300

    without imports

--- Assignment4/Assignment2/src/main/java/com/example/Assignment2/Assignment2Application.java
+++ Assignment4/Assignment2/src/main/java/com/example/Assignment2/Assignment2Application.java
@@ -2,10 +2,6 @@ package com.example.Assignment2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Assignment2Application {
