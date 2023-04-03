06db53524867a5993c1d632ef7d9a11a0b56421f
911_Arba_Catalina <91603584+CatalinaArba@users.noreply.github.com>
2023-04-03 10:15:58 +0300

    Port 16.16.77.214

--- react-frontend-mpp/src/main.tsx
+++ react-frontend-mpp/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-export const GlobalURL=`http://13.53.43.189`
+export const GlobalURL=`http://16.16.77.214`
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
