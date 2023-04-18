967376da0dbeb48faeae420307e2f314e3043486
911_Arba_Catalina <91603584+CatalinaArba@users.noreply.github.com>
2023-04-18 20:13:53 +0300

    new address

--- react-frontend-mpp/src/components.ts
+++ react-frontend-mpp/src/components.ts
@@ -1,2 +1,2 @@
-//export const  BACKEND_API_URL = "/api";
-export const  BACKEND_API_URL ="http://16.170.209.193:80"
+export const  BACKEND_API_URL = "/api";
+//export const  BACKEND_API_URL ="http://16.170.209.193:80"
